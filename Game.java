0a5098a9c348fa777acbd803b0e24bf438fa43bd
Leo Dresang <lrdresang@yahoo.com>
2024-09-15 23:55:16 -0500

    added random turn order assigning

--- Game.java
+++ Game.java
@@ -11,11 +11,43 @@ public class Game {
     int numOfPlayers;
 
     // List of players in a particular game.
-    ArrayList<Player> players;
+    ArrayList<Player> players = new ArrayList<Player>();
 
     // Creates a new game and the player objects.
     public Game(int numOfPlayers, ArrayList<String> names){
         this.numOfPlayers = numOfPlayers;
+
+
+        // Randomly assigning player numbers here.
+        ArrayList<Integer> playerOrder = new ArrayList<Integer>();
+        for(int i = 1; i <= numOfPlayers; i++){
+            playerOrder.add(i);
+        }
+        for(int i = 0; i < names.size(); i++){
+            Player temp = new Player(names.get(i), playerOrder.remove((int)(Math.random() * playerOrder.size())));
+            players.add(temp);
+        }
+
+        // Sorting players by turn order.
+        for(int i = 0; i < players.size(); i ++){
+            for(int j = i+1; j < players.size(); j ++){
+                Player temp;
+                if(players.get(j).playerNum < players.get(i).playerNum){
+                    temp = players.get(i);
+                    players.set(i, players.get(j));
+                    players.set(j, temp);
+                }
+            }
+        }
+
+        // Printing out player orders.
+        System.out.println("-------------------------------------");
+        System.out.println("Randomly assigned player turn order:");
+        for(int i = 0; i < players.size(); i++){
+            System.out.println(players.get(i).playerNum + "  -  " + players.get(i).name);
+        }
+        
+
 
 
     }
--- Main.java
+++ Main.java
@@ -32,13 +32,13 @@ public class Main {
 
         ArrayList<String> names = new ArrayList<String>();
 
+        System.out.println("-------------------------------------");
         System.out.println("Input the name of each player.");
         for (int i = 0; i < numOfPlayers; i++){
             System.out.print("Player " + (i+1) + ": ");
             String temp = input.nextLine();
             names.add(temp);
         }
-
 
         // Create a new game.
         Game game = new Game(numOfPlayers, names);
